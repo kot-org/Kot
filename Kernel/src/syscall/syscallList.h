b4a7d8bf2716877b11293d60bb5862ef49f73e8c
Keep-Konect <keep.konect.main@gmail.com>
2021-12-17 07:48:59 +0100

    Update syscallList.h

--- Kernel/src/syscall/syscallList.h
+++ Kernel/src/syscall/syscallList.h
@@ -2,8 +2,20 @@
 
 #define Sys_CreatShareMemory 0x0
 #define Sys_GetShareMemory 0x1
+
 #define Sys_CreatSubtask 0x2
 #define Sys_ExecuteSubtask 0x3
+
 #define Sys_Exit 0x4
+
 #define Sys_Map 0x5
 #define Sys_Unmap 0x6
+
+#define Sys_IRQRedirect 0x7
+#define Sys_IRQDefault 0x8
+
+#define Sys_GetTaskInfo 0x9
+#define Sys_SetTaskInfo 0xA
+
+#define Sys_CreatThread 0xB
+#define Sys_LaunchThread 0xC
