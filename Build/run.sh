9e8d7c6d3e426536c4b3315422849deaeeb8592a
Camille <45128167+YiraSan@users.noreply.github.com>
2022-06-28 18:03:02 +0200

    Same change for run.sh

--- Build/run.sh
+++ Build/run.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -no-reboot -no-shutdown -M smm=off -serial stdio -machine q35 -cpu qemu64 -smp 8 -cdrom Bin/kot.iso -m 256M
+qemu-system-x86_64 -no-reboot -no-shutdown -M smm=off -serial stdio -machine q35 -cpu qemu64 -smp 8 -cdrom Bin/kot.iso -m 256M -s -S
