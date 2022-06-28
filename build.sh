a95b752dc8d4f465ad0581853f4b17432a6dcf38
Zartov <marlotsebastien01@gmail.com>
2022-06-28 14:06:33 +0200

    Update

---
+++ build.sh
@@ -0,0 +1,28 @@
+echo -e "\e[32mCreating compilation dirs...\e[0m"
+
+mkdir -p Sysroot/include/kot/
+mkdir -p Sysroot/lib/
+mkdir -p Bin/Modules/
+
+echo -e "\e[32mCompiling kot libraries...\e[0m"
+
+cd Sources/Libs/
+
+# libc
+
+cd libc/build/
+make
+cd ../../
+
+# abi
+
+cd abi/build/
+make 
+cd ../../
+
+# kernel
+
+cd ../Kernel/
+echo -e "\e[32mBuilding kernel...\e[0m"
+
+make all
