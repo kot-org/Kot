65f5ee9bbe4c93aad8a0bb70c616a3fbd7ab9321
Camille <mine.vivi.gamer.you@gmail.com>
2022-07-14 17:12:00 +0200

    Upper/Lower case uniformisation

--- Build/build.sh
+++ Build/build.sh
@@ -4,45 +4,38 @@ cd "../"
 
 echo -e "\e[32mCreating compilation dirs...\e[0m"
 
-mkdir -p "Sysroot/include/kot/"
-mkdir -p "Sysroot/lib/"
+mkdir -p "Sysroot/Include/kot/"
+mkdir -p "Sysroot/Lib/"
 mkdir -p "Bin/Modules/"
 
+cp -p "Build/Starter.cfg" "Bin/Modules/Starter.cfg"
+
+cd "Sources/"
+
+# Libraries
 echo -e "\e[32mCompiling kot libraries...\e[0m"
 
-cd "Sources/Libs/"
-
-# libc
-
-cd "libc/build/"
-make
-cd "../../"
-
-# abi
-
-cd "abi/build/"
-make
-cd "../../../"
-
-echo -e "\e[32mCompiling kot drivers...\e[0m"
+sudo make -C "Libs/libc/Build"
+sudo make -C "Libs/abi/Build"
 
 # Drivers
-sudo make -C "Modules/Drivers/PS2/Build"
-sudo make -C "Modules/Drivers/VGA/Build"
+echo -e "\e[32mCompiling kot drivers...\e[0m"
 
-echo -e "\e[32mCompiling kot modules...\e[0m"
+sudo make -C "Modules/Drivers/ps2/Build"
+sudo make -C "Modules/Drivers/vga/Build"
 
 # Services
-sudo make -C "Modules/Services/System/Build"
-sudo make -C "Modules/Services/WM/Build"
+echo -e "\e[32mCompiling kot services...\e[0m"
+
+sudo make -C "Modules/Services/system/Build"
+sudo make -C "Modules/Services/wm/Build"
+sudo make -C "Modules/Services/uisd/Build"
 
 # ramfs
-
 cd "../"
 sudo bash "./Tools/BuildRamFS.sh"
 
 # kernel
-
 cd "./Sources/Kernel/"
 echo -e "\e[32mBuilding kernel...\e[0m"
 
