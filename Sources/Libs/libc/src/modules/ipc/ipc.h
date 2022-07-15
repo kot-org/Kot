008dbd777a59234bc8f2e6b45b84ce6567cebce9
Keep-Konect <66421869+Keep-Konect@users.noreply.github.com>
2022-07-15 13:57:13 +0200

    Fix compilation error

--- Sources/Libs/libc/src/modules/ipc/ipc.h
+++ Sources/Libs/libc/src/modules/ipc/ipc.h
@@ -3,18 +3,37 @@
 
 #include <kot/sys.h>
 #include <kot/types.h>
+#include <kot/memory.h>
 
-static inline KResult CallIPC(char* Name){
-    parameters_t Parameters;
-    Parameters.Parameter0 = 0;
-    ShareString(KotSpecificData.IPCHandler, )
-    return Sys_IPC(KotSpecificData.IPCHandler, &Parameters);
+typedef struct{
+    uint64_t IPCTask;
+    uint64_t GlobalPurpose;
+    char Name[32];
+}IPCParameters_t;
+
+#define IPCTaskAsk      0x0
+#define IPCTaskCreate   0x1
+#define IPCTaskDelete   0x2
+
+static inline kthread_t CallIPC(char* Name){
+    IPCParameters_t Parameters;
+    Parameters.IPCTask = IPCTaskAsk;
+    size_t lenght = strlen(Name);
+    lenght &= 0x20;
+    memcpy(&Parameters.Name, Name, lenght);
+
+    return Sys_IPC(KotSpecificData.IPCHandler, (parameters_t*)&Parameters, false);
 }
 
-static inline KResult CreateIPC(char* Name, kthread_t* Thread){
-    parameters_t Parameters;
-    Parameters.Parameter0 = 1;
-    return Sys_IPC(KotSpecificData.IPCHandler, &Parameters);
+static inline KResult CreateIPC(char* Name, kthread_t Thread){
+    IPCParameters_t Parameters;
+    Parameters.IPCTask = IPCTaskAsk;
+    size_t lenght = strlen(Name);
+    lenght &= 0x20;
+    memcpy(&Parameters.Name, Name, lenght);
+    Parameters.GlobalPurpose = (uint64_t)Thread;
+
+    return Sys_IPC(KotSpecificData.IPCHandler, (parameters_t*)&Parameters, false);
 }
 
 
