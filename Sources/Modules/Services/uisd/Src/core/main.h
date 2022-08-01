ee01aad5518398063aed3c8874836760c8ee6663
Seb <marlotsebastien01@gmail.com>
2022-08-01 17:10:35 +0200

    Update

--- Sources/Modules/Services/uisd/Src/core/main.h
+++ Sources/Modules/Services/uisd/Src/core/main.h
@@ -1,3 +1,25 @@
 #pragma once
 
 #include <kot/sys.h>
+
+typedef struct {
+    kthread_t GetUSBDevice;
+    kthread_t SendUSBPacket;
+    kthread_t ReceiveUSBPacket;
+} usb_t;
+
+typedef struct {
+    kthread_t GetPCIDevice;
+    kthread_t SetupMSIX;
+} pci_t;
+
+typedef struct {
+    kthread_t GetDiskInfo;
+    kthread_t GetPartitionInfo;
+
+    kthread_t ReadDisk;
+    kthread_t WriteDisk;
+
+    kthread_t GetVendorID;
+    kthread_t GetDeviceID;
+} storage_t;
