482c61df5bb85da90e8201a6b8a460b25c84dc09
Seb <marlotsebastien01@gmail.com>
2022-08-01 17:51:38 +0200

    Update

--- Sources/Modules/Services/uisd/Src/core/main.h
+++ Sources/Modules/Services/uisd/Src/core/main.h
@@ -2,6 +2,17 @@
 
 #include <kot/sys.h>
 
+typedef enum {
+    GRAPHICS    = 0,
+    AUDIO       = 1,
+    USB         = 2,
+    STORAGE     = 3,
+    WIFI        = 4,
+    BLUETOOTH   = 5,
+    RGB         = 6,
+    OTHERS      = 255
+} DriverTypeEnum;
+
 typedef struct {
     kthread_t GetUSBDevice;
     kthread_t SendUSBPacket;
@@ -23,3 +34,12 @@ typedef struct {
     kthread_t GetVendorID;
     kthread_t GetDeviceID;
 } storage_t;
+
+typedef struct {
+    kthread_t rename;
+    kthread_t remove;
+    kthread_t fopen;
+    kthread_t mkdir;
+    kthread_t readdir;
+    kthread_t flist;
+} vfs_t;
