bd0131ac82fb7464a1ba7180f3bc0460810f9dd4
YiraSan <mine.vivi.gamer.you@gmail.com>
2022-08-20 12:16:55 +0200

    fix j&s

--- Sources/Modules/Services/jvm/Src/se8/threads/thread.h
+++ Sources/Modules/Services/jvm/Src/se8/threads/thread.h
@@ -14,7 +14,7 @@ namespace SE8 {
         uint64_t pid;
         bool used = false;
         char* name;
-        thread thr;
+        thread_t thr;
     public:
         Thread(uint64_t pid);
         void run(JavaVM* jvm, char* className, char* methodName, char* signature, uint32_t* args, uint16_t args_length);
