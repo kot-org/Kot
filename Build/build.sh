74b3fa56b3933df0fd42aa8d007b0a467c62cb91
Seb <marlotsebastien01@gmail.com>
2022-08-20 20:50:27 +0200

    Update

--- Build/build.sh
+++ Build/build.sh
@@ -31,9 +31,13 @@ sudo make -C "System/Build"
 echo -e "\e[32mCompiling kot drivers...\e[0m"
 
 sudo make -C "Modules/Drivers/pci/Build"
+sudo make -C "Modules/Drivers/usb/uhci/Build"
+
 sudo make -C "Modules/Drivers/ps2/Build"
+
 sudo make -C "Modules/Drivers/vga/Build"
-sudo make -C "Modules/Drivers/pcspeaker/Build"
+
+sudo make -C "Modules/Drivers/pcspk/Build"
 
 # Services
 echo -e "\e[32mCompiling kot services...\e[0m"
