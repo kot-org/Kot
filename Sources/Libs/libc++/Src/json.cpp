0382f472b425b34d054430d35a8c6365826cf0ad
YiraSan <mine.vivi.gamer.you@gmail.com>
2022-08-21 23:14:51 +0200

    implement json parser

---
+++ Sources/Libs/libc++/Src/json.cpp
@@ -0,0 +1,426 @@
+#include "json.h"
+#include "string.h"
+
+// todo: implement back slash support on JsonString
+// todo: implement comments
+// todo: implement serealization of objects
+
+namespace std {
+
+    JsonParser::JsonParser(JsonLexer* lex) {
+
+        lexer = lex;
+
+        lexer->skipUseless();
+
+        switch (lexer->buffer[lexer->index]) {
+            case '{':
+                value = new JsonObject(lexer);
+                break;
+            case '[':
+                value = new JsonArray(lexer);
+                break;
+            case '"':
+                value = new JsonString(lexer);
+                break;
+            case 'n':
+                value = new JsonNull(lexer);
+                break;
+            case 't':
+            case 'f':
+                value = new JsonBoolean(lexer);
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                value = new JsonNumber(lexer);
+                break;
+            default:
+                code = JSON_FAILED;
+                return;
+        }
+
+        code = value->getCode();
+
+    }
+
+    JsonParser::JsonParser(char* str) {
+
+        lexer = (JsonLexer*) malloc(sizeof(JsonLexer));
+        lexer->buffer = str;
+        lexer->index = 0;
+
+        lexer->skipUseless();
+
+        switch (lexer->buffer[lexer->index]) {
+            case '{':
+                value = new JsonObject(lexer);
+                break;
+            case '[':
+                value = new JsonArray(lexer);
+                break;
+            case '"':
+                value = new JsonString(lexer);
+                break;
+            case 'n':
+                value = new JsonNull(lexer);
+                break;
+            case 't':
+            case 'f':
+                value = new JsonBoolean(lexer);
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                value = new JsonNumber(lexer);
+                break;
+            default:
+                code = JSON_FAILED;
+                return;
+        }
+
+        code = value->getCode();
+
+    }
+
+    JsonParsingCode JsonParser::getCode() {
+        return this->code;
+    }
+
+    JsonValue* JsonParser::getValue() {
+        return this->value;
+    }
+
+    JsonNull::JsonNull(JsonLexer* lexer) : JsonValue() {
+        if (lexer->buffer[lexer->index] == 'n') {
+            lexer->index++;
+            if (lexer->buffer[lexer->index] == 'u') {
+                lexer->index++;
+                if (lexer->buffer[lexer->index] == 'l') {
+                    lexer->index++;
+                    if (lexer->buffer[lexer->index] == 'l') {
+                        lexer->index++;
+                        return;
+                    }
+                }
+            }
+        }
+        code = JSON_FAILED;
+    }
+
+    char* JsonNull::serealize() {
+        return "null";
+    }
+
+    JsonParsingCode JsonNull::getCode() {
+        return this->code;
+    }
+
+    JsonTypes JsonNull::getType() {
+        return JSON_NULL;
+    }
+
+    JsonBoolean::JsonBoolean(JsonLexer* lexer) : JsonValue() {
+        if (lexer->buffer[lexer->index] == 't') {
+            lexer->index++;
+            if (lexer->buffer[lexer->index] == 'r') {
+                lexer->index++;
+                if (lexer->buffer[lexer->index] == 'u') {
+                    lexer->index++;
+                    if (lexer->buffer[lexer->index] == 'e') {
+                        lexer->index++;
+                        this->val = true;
+                        return;
+                    }
+                }
+            }
+        } else if (lexer->buffer[lexer->index] == 'f') {
+            lexer->index++;
+            if (lexer->buffer[lexer->index] == 'a') {
+                lexer->index++;
+                if (lexer->buffer[lexer->index] == 'l') {
+                    lexer->index++;
+                    if (lexer->buffer[lexer->index] == 's') {
+                        lexer->index++;
+                        if (lexer->buffer[lexer->index] == 'e') {
+                            lexer->index++;
+                            this->val = false;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        code = JSON_FAILED;
+    }
+
+    char* JsonBoolean::serealize() {
+        if (this->val == true) {
+            return "true";
+        } else if (this->val == false) {
+            return "false";
+        }
+    }
+
+    JsonParsingCode JsonBoolean::getCode() {
+        return this->code;
+    }
+
+    JsonTypes JsonBoolean::getType() {
+        return JSON_BOOLEAN;
+    }
+
+    bool JsonBoolean::get() {
+        return this->val;
+    }
+
+    void JsonBoolean::set(bool val) {
+        this->val = val;
+    }
+
+    JsonNumber::JsonNumber(JsonLexer* lexer) : JsonValue() {
+        // todo: implement float
+        uint16_t start = lexer->index;
+        while (lexer->buffer[lexer->index] >= '0' && lexer->buffer[lexer->index] <= '9') {
+            if (lexer->buffer[lexer->index] == '\0') {
+                code = JSON_FAILED;
+                return;
+            }
+            lexer->index++;
+        }
+        uint16_t size = lexer->index - start;
+        char* temp = (char*) malloc(size);
+        memcpy(temp, (uintptr_t)((uint64_t) lexer->buffer + start), size);
+        this->number = atoi(temp);
+        free(temp);
+    }
+
+    char* JsonNumber::serealize() {
+        char* buffer = (char*) malloc(asi(this->number, 10));
+        return itoa(this->number, buffer, 10);
+    }
+
+    JsonParsingCode JsonNumber::getCode() {
+        return this->code;
+    }
+
+    JsonTypes JsonNumber::getType() {
+        return JSON_NUMBER;
+    }
+
+    int32_t JsonNumber::get() {
+        return this->number;
+    }
+
+    void JsonNumber::set(int32_t num) {
+        this->number = num;
+    }
+
+    JsonString::JsonString(JsonLexer* lexer) : JsonValue() {
+        if (lexer->buffer[lexer->index] != '"') {
+            code = JSON_FAILED;
+            return;
+        }
+        lexer->index++;
+        uint64_t start = lexer->index;
+        while (lexer->buffer[lexer->index] != '"') {
+            if (lexer->buffer[lexer->index] == '\0') {
+                code = JSON_FAILED;
+                return;
+            }
+            lexer->index++;
+        }
+        uint64_t size = lexer->index - start;
+        this->buffer = (char*) malloc(size);
+        memcpy(this->buffer, (uintptr_t)((uint64_t) lexer->buffer + start), size);
+        lexer->index++;
+    }
+
+    char* JsonString::serealize() {
+        StringBuilder* builder = new StringBuilder("\"");
+        builder->append(buffer);
+        builder->append("\"");
+        return builder->toString();
+    }
+
+    JsonParsingCode JsonString::getCode() {
+        return this->code;
+    }
+
+    JsonTypes JsonString::getType() {
+        return JSON_STRING;
+    }
+
+    char* JsonString::get() {
+        return this->buffer;
+    }
+
+    void JsonString::set(char* str) {
+        this->buffer = str;
+    }
+
+    JsonObject::JsonObject(JsonLexer* lexer) : JsonValue() {
+        if (lexer->buffer[lexer->index] != '{') {
+            code = JSON_FAILED;
+            return;
+        }
+        lexer->index++;
+        if (lexer->buffer[lexer->index] == '}') {
+            lexer->index++;
+            return;
+        }
+        while (true) {
+            if (lexer->buffer[lexer->index] == '\0') {
+                code = JSON_FAILED;
+                return;
+            }
+            JsonParser* parser1 = new JsonParser(lexer);
+            if (parser1->getCode() == JSON_FAILED || parser1->getValue()->getType() != JSON_STRING) {
+                code = JSON_FAILED;
+                return;
+            }
+            JsonString* key = (JsonString*) parser1->getValue();
+            lexer->skipUseless();
+            if (lexer->buffer[lexer->index] != ':') {
+                code = JSON_FAILED;
+                return;
+            }
+            lexer->index++;
+            JsonParser* parser2 = new JsonParser(lexer);
+            if (parser2->getCode() == JSON_FAILED) {
+                code = JSON_FAILED;
+                return;
+            }
+            map_set(this->obj, key->get(), parser2->getValue());
+            lexer->skipUseless();
+            if (lexer->buffer[lexer->index] == '}') {
+                lexer->index++;
+                break;
+            } else if (lexer->buffer[lexer->index] == ',') {
+                lexer->index++;
+            } else {
+                code = JSON_FAILED;
+                return;
+            }
+        }
+    }
+
+    char* JsonObject::serealize() {
+        StringBuilder* builder = new StringBuilder("{");
+        for (uint64_t i = 0; i < obj->length; i++) {
+            char* key = (char*) map_key(obj, i);
+            builder->append("\"");
+            builder->append(key);
+            builder->append("\":");
+            JsonValue* val = (JsonValue*) map_geti(obj, i);
+            char* deserialized = val->serealize();
+            builder->append(deserialized);
+            if (val->getType() == JSON_STRING || val->getType() == JSON_NUMBER) {
+                free(deserialized);
+            }
+            if (i != obj->length-1) {
+                builder->append(",");
+            }
+        }
+        builder->append("}");
+        return builder->toString();
+    }
+
+    JsonParsingCode JsonObject::getCode() {
+        return this->code;
+    }
+
+    JsonTypes JsonObject::getType() {
+        return JSON_OBJECT;
+    }
+
+    JsonArray::JsonArray(JsonLexer* lexer) : JsonValue() {
+        if (lexer->buffer[lexer->index] != '[') {
+            code = JSON_FAILED;
+            return;
+        }
+        lexer->index++;
+        if (lexer->buffer[lexer->index] == ']') {
+            lexer->index++;
+            return;
+        }
+        while (true) {
+            if (lexer->buffer[lexer->index] == '\0') {
+                code = JSON_FAILED;
+                return;
+            }
+            JsonParser* parser = new JsonParser(lexer);
+            if (parser->getCode() == JSON_FAILED) {
+                code = JSON_FAILED;
+                return;
+            }
+            vector_push(this->arr, parser->getValue());
+            lexer->skipUseless();
+            if (lexer->buffer[lexer->index] == ']') {
+                lexer->index++;
+                break;
+            } else if (lexer->buffer[lexer->index] == ',') {
+                lexer->index++;
+            } else {
+                code = JSON_FAILED;
+                return;
+            }
+        }
+    }
+
+    JsonValue* JsonArray::get(uint64_t index) {
+        return (JsonValue*) vector_get(this->arr, index);
+    }
+
+    void JsonArray::set(uint64_t index, JsonValue* value) {
+        vector_set(this->arr, index, value);
+    }
+
+    void JsonArray::push(JsonValue* value) {
+        vector_push(this->arr, value);
+    }
+
+    uint64_t JsonArray::length() {
+        return this->arr->length;
+    }
+
+    char* JsonArray::serealize() {
+        StringBuilder* builder = new StringBuilder("[");
+        for (uint64_t i = 0; i < length(); i++) {
+            if (i != 0) {
+                builder->append(",");
+            }
+            JsonValue* val = get(i);
+            char* deserialized = val->serealize();
+            builder->append(deserialized);
+            if (val->getType() == JSON_STRING || val->getType() == JSON_NUMBER) {
+                free(deserialized);
+            }
+        }
+        builder->append("]");
+        return builder->toString();
+    }
+
+    JsonParsingCode JsonArray::getCode() {
+        return this->code;
+    }
+
+    JsonTypes JsonArray::getType() {
+        return JSON_ARRAY;
+    }
+
+}
---
+++ Sources/Libs/libc++/Src/json.h
@@ -0,0 +1,142 @@
+#pragma once
+
+#include <kot/heap.h>
+#include <kot/cstring.h>
+#include <kot/utils/map.h>
+
+namespace std {
+
+    enum JsonTypes : uint8_t {
+        JSON_NULL = 0,
+        JSON_NUMBER = 1,
+        JSON_STRING = 2,
+        JSON_BOOLEAN = 3,
+        JSON_ARRAY = 4,
+        JSON_OBJECT = 5,
+    };
+
+    struct JsonLexer {
+
+        uint64_t index;
+        char* buffer;
+
+        void skipUseless() {
+            while (buffer[index] == ' ' || buffer[index] == '\n' || buffer[index] == '\r') {
+                index++;
+            }
+        }
+
+    };
+
+    enum JsonParsingCode {
+        JSON_SUCCESS,
+        JSON_FAILED,
+    };
+
+    class JsonValue {
+    private:
+    public:
+        JsonValue() {}
+        virtual ~JsonValue() {}
+        virtual char* serealize() = 0;
+        virtual JsonTypes getType() = 0;
+        virtual JsonParsingCode getCode() = 0;
+    };
+
+    class JsonNumber : public JsonValue {
+    private:
+        JsonParsingCode code = JSON_SUCCESS;
+        int32_t number;
+    public:
+        JsonNumber() : JsonValue() {}
+        JsonNumber(JsonLexer* lexer);
+        char* serealize();
+        JsonTypes getType();
+        JsonParsingCode getCode();
+        int32_t get();
+        void set(int32_t num);
+        ~JsonNumber() {}
+    };
+
+    class JsonString : public JsonValue {
+    private:
+        char* buffer;
+        JsonParsingCode code = JSON_SUCCESS;
+    public:
+        JsonString() : JsonValue() {}
+        JsonString(JsonLexer* lexer);
+        char* serealize();
+        JsonTypes getType();
+        JsonParsingCode getCode();
+        char* get();
+        void set(char* str);
+        ~JsonString() {}
+    };
+
+    class JsonBoolean : public JsonValue {
+    private:
+        JsonParsingCode code = JSON_SUCCESS;
+        bool val;
+    public:
+        JsonBoolean() : JsonValue() {}
+        JsonBoolean(JsonLexer* lexer);
+        char* serealize();
+        JsonTypes getType();
+        JsonParsingCode getCode();
+        bool get();
+        void set(bool val);
+        ~JsonBoolean() {}
+    };
+
+    class JsonObject : public JsonValue {
+    private:
+        vector_t* obj = map_create();
+        JsonParsingCode code = JSON_SUCCESS;
+    public:
+        JsonObject(JsonLexer* lexer);
+        char* serealize();
+        JsonTypes getType();
+        JsonParsingCode getCode();
+        ~JsonObject() {}
+    };
+
+    class JsonNull : public JsonValue {
+    private:
+        JsonParsingCode code = JSON_SUCCESS;
+    public:
+        JsonNull(JsonLexer* lexer);
+        char* serealize();
+        JsonTypes getType();
+        JsonParsingCode getCode();
+        ~JsonNull() {}
+    };
+
+    class JsonArray : public JsonValue {
+    private:
+        vector_t* arr = vector_create();
+        JsonParsingCode code = JSON_SUCCESS;
+    public:
+        JsonArray(JsonLexer* lexer);
+        char* serealize();
+        JsonTypes getType();
+        JsonParsingCode getCode();
+        JsonValue* get(uint64_t index);
+        void set(uint64_t index, JsonValue* value);
+        void push(JsonValue* value);
+        uint64_t length();
+        ~JsonArray() {}
+    };
+
+    class JsonParser {
+    private:
+        JsonLexer* lexer = NULL;
+        JsonValue* value = NULL;
+        JsonParsingCode code = JSON_SUCCESS;
+    public:
+        JsonParser(JsonLexer* lexer);
+        JsonParser(char* source);
+        JsonParsingCode getCode();
+        JsonValue* getValue();
+    };
+
+}
