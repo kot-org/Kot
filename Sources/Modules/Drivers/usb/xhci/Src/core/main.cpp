69f253d419eac758fb7d6f5314cbc75cb18c1f60
Keep-Konect <66421869+Keep-Konect@users.noreply.github.com>
2022-08-23 08:01:49 +0200

    Add usb drivers (WIP)

---
+++ Sources/Modules/Drivers/usb/xhci/Src/core/main.cpp
@@ -0,0 +1,11 @@
+#include <core/main.h>
+
+extern "C" int main(int argc, char* argv[]) {
+    Printlog("[UHCI] Initialization ...");
+
+    
+
+    Printlog("[UHCI] Driver initialized successfully");
+
+    return KSUCCESS;
+}
