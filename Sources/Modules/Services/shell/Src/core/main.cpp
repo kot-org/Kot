a7e0d8d9b3b8e08177f25c8d38346c5a2b2d2b16
YiraSan <mine.vivi.gamer.you@gmail.com>
2022-08-27 02:00:01 +0200

    more little

--- Sources/Modules/Services/shell/Src/core/main.cpp
+++ Sources/Modules/Services/shell/Src/core/main.cpp
@@ -5,7 +5,7 @@ extern "C" int main() {
     process_t self = NULL;
     Sys_GetProcessKey(&self);
 
-    uint32_t wid = orb::Create(500, 500, 10, 10);
+    uint32_t wid = orb::Create(300, 300, 10, 10);
     uintptr_t fb = orb::GetFramebuffer(wid);
 
 }
