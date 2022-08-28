a1374cff43e76a91da052d9dcdf65c30b37f90ec
Keep-Konect <66421869+Keep-Konect@users.noreply.github.com>
2022-08-28 14:15:32 +0200

    Remove ssfn

--- Sources/Libs/kot-ui/Src/font.h
+++ Sources/Libs/kot-ui/Src/font.h
@@ -18,35 +18,10 @@ typedef struct {
     uint64_t pitch;
 } font_fb_t;
 
-enum font_family{
-    font_family_serif = 0,
-    font_family_sans = 1,
-    font_family_decor = 2,
-    font_family_monospace = 3,
-    font_family_hand = 4,
-    font_family_byname = 5,
-};
-
-
-#define font_style_regular 1 << 0x0
-#define font_style_bold 1 << 0x1
-#define font_style_italic 1 << 0x2
-#define font_style_usrdef2 1 << 0x3
-#define font_style_underline 1 << 0x4
-#define font_style_sthrough 1 << 0x5
-#define font_style_a 1 << 0x6
-#define font_style_noaa 1 << 0x7
-#define font_style_nosmooth 1 << 0x8
-#define font_style_nokern 1 << 0x9
-#define font_style_nodefglyph 1 << 0xA
-#define font_style_nocache 1 << 0xB
-#define font_style_rtl 1 << 0xC
-#define font_style_ab_size 1 << 0xD
-
-kfont_t* LoadFont(uintptr_t data, enum font_family family, const char* name, int style, int size);
+kfont_t* LoadFont(uintptr_t data);
 void FreeFont(kfont_t* font);
 
-void PrintFont(kfont_t* font, char* str, font_fb_t* buffer, uint64_t x, uint64_t y);
+void PrintFont(kfont_t* font, char* str, font_fb_t* buffer, uint64_t x, uint64_t y, uint32_t color);
 
 #if defined(__cplusplus)
 }
--- Sources/Modules/Services/shell/Src/core/main.cpp
+++ Sources/Modules/Services/shell/Src/core/main.cpp
@@ -11,15 +11,16 @@ extern "C" int main() {
 
     srv_system_fileheader_t* File = (srv_system_fileheader_t*)malloc(sizeof(srv_system_fileheader_t));
     srv_system_callback_t* Data = Srv_System_ReadFileInitrd("default-font.sfn", File, true);
-
-    kfont_t* font = LoadFont(File->Data, font_family_serif, "default-font", NULL, 64);
+    
+    Printlog("[Shell]Ok");
+    kfont_t* font = LoadFont(File->Data, font_family_serif, NULL, NULL, 10);
     
     font_fb_t buffer;
     buffer.address = fb;
     buffer.width = 300;
     buffer.height = 300;
     buffer.pitch = 300 * 4;
-    PrintFont(font, "Hello", &buffer, 0, 0);
+    PrintFont(font, "Hello", &buffer, 0, 0, 0xffffff);
     FreeFont(font);
     Printlog("[Shell]End");
 
