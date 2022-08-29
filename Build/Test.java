49cff6fe1c8f87f71c048aa67cb0e0318746f6e6
YiraSan <mine.vivi.gamer.you@gmail.com>
2022-08-29 15:49:16 +0200

    edit Test.class

--- Build/Test.java
+++ Build/Test.java
@@ -3,10 +3,7 @@ public class Test {
     public static int[] arr = new int[100000];
 
     public static void main(String[] args) {
-        String a2 = "test" + 1230;
-        System.out.println(a2);
         System.out.println("hello world!");
-        System.out.println("ça va konect tt va bien ?");
         arr[0] = 20 * 20;
         arr[1] = 10 * 2;
         arr[99998] = 99;
