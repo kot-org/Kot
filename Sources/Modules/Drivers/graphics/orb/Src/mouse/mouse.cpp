fb27d8dcf0c2a7899672ef2371f7bbe5975094e6
Keep-Konect <66421869+Keep-Konect@users.noreply.github.com>
2022-10-31 18:12:15 +0100

    Update mouse.cpp

--- Sources/Modules/Drivers/graphics/orb/Src/mouse/mouse.cpp
+++ Sources/Modules/Drivers/graphics/orb/Src/mouse/mouse.cpp
@@ -3,15 +3,19 @@
 Point_t CursorPosition;
 Point_t CursorMaxPosition;
 
-uint8_t Width;
-uint8_t Height;
+int64_t Width;
+int64_t Height;
 
 thread_t MouseRelativeInterrupt;
 
 void InitializeCursor(){
     CursorPosition.x = 0;
     CursorPosition.y = 0;
+    Width = CursorWidth;
+    Height = CursorHeight;
+
     Sys_Createthread(Sys_GetProcess(), (uintptr_t)&CursorInterrupt, PriviledgeApp, NULL, &MouseRelativeInterrupt);
+
     BindMouseRelative(MouseRelativeInterrupt, false);
 }
 
@@ -41,18 +45,22 @@ void CursorInterrupt(int64_t x, int64_t y, int64_t z, uint64_t status){
 
     if(Width > CursorWidth){
         Width = CursorWidth;
+    }else if(Width < 0){
+        Width = 0;
     }
 
     if(Height > CursorHeight){
         Height = CursorHeight;
+    }else if(Height < 0){
+        Height = 0;
     }
 
     Sys_Event_Close();
 }
 
 void DrawCursor(Graphic::framebuffer_t* fb, uint8_t* Mask, uint32_t Color[CursorHeight][CursorWidth]) {    
-    for(uint8_t y = 0; y < Height; y++) {
-        for(uint8_t x = 0; x < Width; x++) {
+    for(uint64_t y = 0; y < Height; y++) {
+        for(uint64_t x = 0; x < Width; x++) {
             uint16_t bit = y * 16 + x;
             uint16_t byte = bit / 8;
 
