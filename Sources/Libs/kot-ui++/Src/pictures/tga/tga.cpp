6c4f96aa4e2f2dfe92d4717b6284f01020845ab8
S3B <marlotsebastien01@gmail.com>
2022-12-06 20:37:06 +0100

    Update TGA

--- Sources/Libs/kot-ui++/Src/pictures/tga/tga.cpp
+++ Sources/Libs/kot-ui++/Src/pictures/tga/tga.cpp
@@ -6,58 +6,69 @@ namespace Ui {
 
     TGA::TGA(char* path, Component* cpnt) {
         srv_system_callback_t* file = Srv_System_ReadFileInitrd(path, true);
-        tgaHeader_t *data = (tgaHeader_t*) file->Data;
+        tgaHeader_t *image = (tgaHeader_t*) file->Data;
 
-        if(!data) return;
+        if(!image) return;
+        if(image->width == 0 || image->height == 0) return;
+
+        uint8_t Bpp = image->bpp;
+        uint8_t Btpp = image->bpp/8;
+        uint32_t Width = image->width,
+                 cpntWidth = cpnt->getStyle()->width;
+        uint32_t Height = image->height,
+                 cpntHeight = cpnt->getStyle()->height;
+        uint32_t Pitch = Width * Btpp;
+        bool isReversed = !(image->imageDescriptor & (1 << 5));
+
+        /* Todo: dessiner l'image dans la fonction draw */
+
+        if(cpntWidth == NULL)
+            cpntWidth = Width;
+        else
+            Width = cpntWidth;
         
-        uint8_t btpp = data->bpp/8;
-        uint32_t pitch = data->width * btpp;
+        if(cpntHeight == NULL)
+            cpntHeight = Height;
+        else
+            Height = cpntHeight;
+        
+        cpnt->updateFramebuffer(Width, Height);
 
-        uint64_t Bpp = 32; 
-        uint64_t Btpp = Bpp / 4; 
-        uint64_t Height = data->height; // Todo user arguments
-        uint64_t Width = data->width; // Todo user arguments
-        uint64_t Pitch = Width * Btpp;
-
-        cpnt->getFramebuffer()->size = Pitch * (Height + 1);
-        cpnt->getFramebuffer()->addr = calloc(cpnt->getFramebuffer()->size);
-        cpnt->getFramebuffer()->pitch = Pitch;
-        cpnt->getFramebuffer()->width = data->width;
-        cpnt->getFramebuffer()->height = data->height;
-        cpnt->getFramebuffer()->bpp = Bpp;
-        cpnt->getFramebuffer()->btpp = Bpp / 8;
-
-        cpnt->getStyle()->width = data->width;
-        cpnt->getStyle()->height = data->height;
-
-        switch (data->imageType)
+        switch (image->imageType)
         {
             case 1:
-
+                Printlog("tga type 1");
                 break;
             
             case 2:
             {
-                uint64_t imageDataOffset = data->colorMapOrigin + data->colorMapLength + 18;
+                Printlog("tga type 2");
+                uintptr_t imageDataOffset = (uintptr_t) (image->colorMapOrigin + image->colorMapLength + 18),
+                          imagePixelData = (uintptr_t) ((uint64_t)image + (uint64_t)imageDataOffset);
 
                 for(uint64_t y = 0; y < Height; y++) {
+                    uint32_t YPos = ((isReversed) ? Height-y-1 : y) * image->height / Height;
+
                     for(uint64_t x = 0; x < Width; x++) {
-                        uint8_t R = *(uint8_t*) ((uint64_t)data+imageDataOffset+x*btpp+pitch*y + 2);
-                        uint8_t G = *(uint8_t*) ((uint64_t)data+imageDataOffset+x*btpp+pitch*y + 1);
-                        uint8_t B = *(uint8_t*) ((uint64_t)data+imageDataOffset+x*btpp+pitch*y + 0);
+                        uint32_t XPos = x * image->width / Width;
+
+                        uint8_t R = *(uint8_t*) ((uint64_t)imagePixelData+XPos*Btpp+Pitch*YPos + 2);
+                        uint8_t G = *(uint8_t*) ((uint64_t)imagePixelData+XPos*Btpp+Pitch*YPos + 1);
+                        uint8_t B = *(uint8_t*) ((uint64_t)imagePixelData+XPos*Btpp+Pitch*YPos + 0);
                         uint32_t Pixel = B | (G << 8) | (R << 16);
-                        putPixel(cpnt->getFramebuffer(), cpnt->getStyle()->x+x, cpnt->getStyle()->y+Height-y, Pixel);
+                        
+                        putPixel(cpnt->getFramebuffer(), cpnt->getStyle()->x+x, cpnt->getStyle()->y+y, Pixel);
                     }
                 }
 
                 break;
             }
             case 9:
-            
+                Printlog("tga type 9");
                 break;
             
             case 10:
-            
+                Printlog("tga type 10");
                 break;
         }
     }
