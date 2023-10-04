16eda7f899ca967ed2f8069a5731f39c60a1d1d6
Keep-Konect <66421869+Keep-Konect@users.noreply.github.com>
2023-10-04 08:51:01 +0200

    Update keyboard translation

--- sources/core/modules/hid/source/interface.c
+++ sources/core/modules/hid/source/interface.c
@@ -1,9 +1,22 @@
+#define SUB_TABLE_DEFAULT_INDEX                 0x0
+#define SUB_TABLE_SHIFT_INDEX                   0x1
+#define SUB_TABLE_ALT_GR_INDEX                  0x2
+#define SUB_TABLE_SHIFT_AND_ALT_GR_INDEX        0x3
+#define SUB_TABLE_COUNT                         0x4
+
+#define TSK_SHIFT       0xF
+#define TSK_CAPS_LOCK   0x11
+#define TSK_ALT_GR      0x13
+
 hid_handler_t hid_internal_handler;
 
 key_handler_t handler;
 spinlock_t scancode_translation_table_lock = {};
 uint16_t* scancode_table_buffer = NULL;
 size_t scancode_table_size = 0;
+
+uint16_t translated_key_shift_start = 0;
+uint16_t translated_key_alt_gr_start = 0;
 
 void key_handler_stub(uint64_t scancode, uint16_t translated_key, bool is_pressed){
     #ifdef DEBUG_KEYBOARD
@@ -16,15 +29,35 @@ void key_handler_stub(uint64_t scancode, uint16_t translated_key, bool is_presse
 }
 
 int key_update_state(uint64_t scancode, bool is_pressed){
+    static bool is_caps_lock = false;
+    static bool is_shift_pressed = false;
+    static bool is_alt_gr_pressed = false;
+
     uint16_t translated_key = 0;
 
     spinlock_acquire(&scancode_translation_table_lock);
+
+    if(is_caps_lock ^ is_shift_pressed){
+        scancode = scancode + translated_key_shift_start;
+    }
+    
+    if(is_alt_gr_pressed){
+        scancode = scancode + translated_key_alt_gr_start;
+    }
 
     if(scancode < scancode_table_size){
         translated_key = scancode_table_buffer[scancode];
     }
 
     spinlock_release(&scancode_translation_table_lock);
+
+    if(translated_key == TSK_CAPS_LOCK && is_pressed){
+        is_caps_lock = !is_caps_lock;
+    }else if(translated_key == TSK_SHIFT){
+        is_shift_pressed = is_pressed;
+    }else if(translated_key == TSK_ALT_GR){
+        is_alt_gr_pressed = is_pressed;
+    }
 
     handler(scancode, translated_key, is_pressed);
 
@@ -40,6 +73,8 @@ int update_scancode_translation_table(uint16_t* table, size_t size){
     spinlock_acquire(&scancode_translation_table_lock);
     scancode_table_buffer = table;
     scancode_table_size = size;
+    translated_key_shift_start = size * SUB_TABLE_SHIFT_INDEX / SUB_TABLE_COUNT;
+    translated_key_alt_gr_start = size * SUB_TABLE_ALT_GR_INDEX / SUB_TABLE_COUNT;
     spinlock_release(&scancode_translation_table_lock);
     return 0;
 }
