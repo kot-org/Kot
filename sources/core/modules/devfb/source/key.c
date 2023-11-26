4ae1dddd012e4912afbbeff81d7f3de2bcac917d
Keep-Konect <66421869+konect-V@users.noreply.github.com>
2023-11-26 20:05:09 +0100

    Update buffer handler

--- sources/core/modules/devfb/source/key.c
+++ sources/core/modules/devfb/source/key.c
@@ -7,36 +7,42 @@
 #include <global/console.h>
 #include <global/scheduler.h>
 
-uint64_t last_scancode;
-bool is_last_scancode = false;
+#define SCANCODE_BUFFER_LENGTH  512
+
+uint64_t scancode_buffer_index_read = 0;
+uint64_t scancode_buffer_index_write = 0;
+size_t scancode_size_to_read = 0;
+uint64_t scancode_buffer[SCANCODE_BUFFER_LENGTH];
 spinlock_t key_handler_lock = SPINLOCK_INIT;
 
 void key_handler(uint64_t scancode, uint16_t translated_key, bool is_pressed){
     spinlock_acquire(&key_handler_lock);
 
-    last_scancode = scancode | ((uint64_t)is_pressed << 63);
-    is_last_scancode = true;
+    scancode_buffer[scancode_buffer_index_write] = (scancode | ((uint64_t)is_pressed << 63));
+    scancode_buffer_index_write = (scancode_buffer_index_write + 1) % SCANCODE_BUFFER_LENGTH;
+    scancode_size_to_read++;
+
+    if(scancode_size_to_read > SCANCODE_BUFFER_LENGTH){
+        scancode_size_to_read = SCANCODE_BUFFER_LENGTH;
+        log_warning("Buffer owerflow");
+    }
 
     spinlock_release(&key_handler_lock);
 }
 
 int fb_interface_read(void* buffer, size_t size, size_t* bytes_read, struct kernel_file_t* file){
-    if(spinlock_test_and_acq(&key_handler_lock)){
-        size_t size_read = 0;
+    spinlock_acquire(&key_handler_lock);
 
-        if(is_last_scancode){
-            *((uint64_t*)buffer) = last_scancode;
-            is_last_scancode = false;
+    if(scancode_size_to_read > 0){
+        *((uint64_t*)buffer) = scancode_buffer[scancode_buffer_index_read];
+        scancode_buffer_index_read = (scancode_buffer_index_read + 1) % SCANCODE_BUFFER_LENGTH;
 
-            *bytes_read = 1;
-        }else{
-            *bytes_read = 0;
-        }
-
-        spinlock_release(&key_handler_lock);
-        return 0;
+        scancode_size_to_read--;
+        *bytes_read = 1;
     }else{
         *bytes_read = 0;
-        return EIO;
     }
+
+    spinlock_release(&key_handler_lock);
+    return 0;
 }
